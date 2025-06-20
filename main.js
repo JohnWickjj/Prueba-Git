988966ae4fe236765314c9c5ad0bb99d3ff79cbc
Jhon <jhonnjairo1@hotmail.com>
2025-06-19 23:51:01 -0500

    tercer commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // Este código se ejecutará una vez que el DOM (Document Object Model) esté completamente cargado.
 document.addEventListener('DOMContentLoaded', () => {
     console.log('¡La página ha sido cargada completamente!');
-    alert('¡Bienvenido a mi increíble sitio web!');
+    alert('¡Bienvenido a mi increíble sitio web! chido');
 
     // También puedes manipular el contenido HTML
     const tituloPrincipal = document.querySelector('h1');
